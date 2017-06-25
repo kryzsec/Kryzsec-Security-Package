c0e2df16aeec32ac8891e358211b891828d266a4
kryzsec <kryzsec@gmail.com>
2017-06-24 21:02:30 -0600

    Add files via upload
    A password generator I developed for S&G but soon found use out of it.

---
+++ passgen.py
@@ -0,0 +1,12 @@
+from random import randint
+_MIN_ = 8
+_MAX_ = 32
+
+charset = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+plength = randint(_MIN_,_MAX_)
+password = ""
+for i in range(0,plength):
+	password = password + charset[randint(0,len(charset)-1)]
+
+print password
